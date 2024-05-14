7555e259788fe61edde59a42cb1199a1d0350b00
Eternal-could <20578466@qq.com>
2024-05-14 21:52:13 +0800

    feat: eslint configuration and fix code burrs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 export function App() {
-
+  let a = 1
+  a = 2
   return (
     <div>
       App
     </div>
   )
 }
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import { App } from './App'
 
 const div = document.getElementById('root') as HTMLElement
 const root = ReactDOM.createRoot(div)
@@ -8,5 +8,5 @@ const root = ReactDOM.createRoot(div)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
